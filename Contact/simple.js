3e6e380d9aad6c9110f6b7fcc0bb55c1e079e2fd
Lauren Leohner <laurenleohner@gmail.com>
2017-06-07 17:41:23 -0400

    updated portfolio site with bootstrap theme

--- Contact/simple.js
+++
@@ -1,7 +0,0 @@
-//simpler version of js form
-function outputName() {
-  var x, y, fname;
-  x= document.getElementById("contactform");
-  y= x.elements["firname"].value;
-  document.getElementById("reply").innerHTML = "Hello, "+y+", thanks for reaching out!";
-}
